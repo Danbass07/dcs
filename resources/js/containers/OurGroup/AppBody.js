112a027f56da75f6745c09e8997fd1e9c346ccaf
Danbass07 <danbass666@googlemail.com>
2019-12-04 12:20:26 +0000

    UPDATE

--- resources/js/containers/OurGroup/AppBody.js
+++ resources/js/containers/OurGroup/AppBody.js
@@ -3,37 +3,20 @@ import styled from "styled-components";
 import Logo from "./components/Logo";
 import InfoDisplay from "./components/InfoDisplay";
 import Buttons from "./components/Buttons";
-<<<<<<< HEAD
-=======
 import Screen from "./components/Screen";
 import LeftMenu from "./components/LeftMenu";
 import RightMenu from "./components/RightMenu";
 import TopMenu from "./components/TopMenu";
 import BottomMenu from "./components/BottomMenu";
->>>>>>> b8fc883c872e36750bd0b7a0a0d5474391ad7b9d
 
 const AppBody = props => {
     const MainWrapper = styled.div`
         height: 600px;
-<<<<<<< HEAD
-        width: 100%;
-=======
         width: 510px;;
->>>>>>> b8fc883c872e36750bd0b7a0a0d5474391ad7b9d
         background-color: black;
     `;
 
     const TopSection = styled.div`
-<<<<<<< HEAD
-    height: 160px;
-    width: 100%;
-    display: grid;
-    grid-template-columns: repeat(5, 1fr);
-    grid-template-rows: repeat(2, 1fr);
-    grid-column-gap: 0px;
-    grid-row-gap: 0px;
-
-=======
         height: 160px;
         width: 510px;
         display: grid;
@@ -51,27 +34,10 @@ const AppBody = props => {
         flex-direction: row;
         left: -380px;
     
->>>>>>> b8fc883c872e36750bd0b7a0a0d5474391ad7b9d
     `;
 
     return (
         <MainWrapper>
-<<<<<<< HEAD
-            <TopSection>
-                <Logo theme={props.theme} />
-                <InfoDisplay theme={props.theme} />
-                <Buttons 
-                theme={props.theme}
-                changeTheme={(themeNumber) => props.changeTheme(themeNumber)}
-                />
-            </TopSection>
-
-            <div>Screen</div>
-            <div>Left Menu</div>
-            <div>Right Menu</div>
-            <div>Top Menu</div>
-            <div>Bottom Menu</div>
-=======
             <div>
             <TopMenu theme={props.theme} />
             </div>
@@ -94,7 +60,6 @@ const AppBody = props => {
             </MiddleSection>
 
             <BottomMenu theme={props.theme} />
->>>>>>> b8fc883c872e36750bd0b7a0a0d5474391ad7b9d
         </MainWrapper>
     );
 };
